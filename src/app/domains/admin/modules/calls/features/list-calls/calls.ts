f010e139936ae2df1e1b9b734ba758e2ef1302f7
Wilfried Musanzi <musanziw.m@gmail.com>
2026-08-16 10:33:05 +0200

    feat: add calls management component with search and pagination functionality

---
+++ src/app/domains/admin/modules/calls/features/list-calls/calls.ts
@@ -0,0 +1,35 @@
+import { DatePipe } from '@angular/common';
+import { HttpParams, httpResource } from '@angular/common/http';
+import { Component, computed, debounced, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { CallRow, QueryParams } from '../../interfaces/calls.interface';
+
+@Component({
+  imports: [DatePipe, FormsModule, MatButtonModule, MatIconModule, MatPaginatorModule, MatTableModule],
+  templateUrl: './calls.html'
+})
+export default class Calls {
+  readonly page = signal<number>(1);
+  readonly q = signal<string>('');
+
+  readonly debouncedQuery = debounced(this.q, 300);
+  readonly displayedColumns = ['name', 'period', 'solutionsCount', 'status'];
+
+  private readonly queryParams = computed<QueryParams>(() => ({
+    page: String(this.page()),
+    q: this.debouncedQuery.value()
+  }));
+
+  readonly callsResource = httpResource<{ data: [CallRow[], number] }>(() => ({
+    url: '/calls',
+    params: this.queryParams() as unknown as HttpParams
+  }));
+
+  onPageChange(event: PageEvent): void {
+    this.page.set(event.pageIndex + 1);
+  }
+}
