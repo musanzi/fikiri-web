331d1779caf3b3a2a1308f060a6a1a0cdc940449
Wilfried Musanzi <musanziw.m@gmail.com>
2026-08-16 10:30:41 +0200

    fix: update server routes to use Client render mode for all paths

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,15 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'auth/**',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'admin/**',
-    renderMode: RenderMode.Client,
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Server,
-  },
+    renderMode: RenderMode.Client
+  }
 ];
