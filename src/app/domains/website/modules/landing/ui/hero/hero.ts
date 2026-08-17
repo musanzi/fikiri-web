74414c7a1575cfb560b4075e59efdc6f6a718385
Wilfried Musanzi <musanziw.m@gmail.com>
2026-08-17 10:36:10 +0200

    feat: enhance hero component with latest call display and improved layout

--- src/app/domains/website/modules/landing/ui/hero/hero.ts
+++ src/app/domains/website/modules/landing/ui/hero/hero.ts
@@ -1,14 +1,26 @@
+import { DatePipe } from '@angular/common';
+import { httpResource } from '@angular/common/http';
 import { Component, computed, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ICallSolution } from '@/app/core/interfaces';
 import { AuthStore } from '@/app/domains/auth/data-access/auth.store';
+import { environment } from '@/environments/environment';
 
 @Component({
   selector: 'hero',
-  imports: [RouterLink],
+  imports: [DatePipe, RouterLink],
   templateUrl: './hero.html'
 })
 export class HeroComponent {
   private readonly authStore = inject(AuthStore);
+
+  protected readonly currentCallsResource = httpResource<{ data: ICallSolution[] }>(() => '/calls/find/current');
+
+  protected readonly latestCall = computed(() => this.currentCallsResource.value()?.data[0]);
+  protected readonly latestCallCoverUrl = computed(() => {
+    const cover = this.latestCall()?.cover;
+    return cover ? `${environment.apiUrl}/uploads/calls/covers/${cover}` : '/images/no-img.png';
+  });
 
   protected readonly submitSolutionLink = computed(() =>
     this.authStore.user() ? '/submit-solution' : '/auth/sign-in'
